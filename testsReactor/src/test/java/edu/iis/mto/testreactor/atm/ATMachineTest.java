73d4b81cbf8c148e3c5e5bcc2da8ab2efffa1379
Magdalena Drozdz <215707@edu.p.lodz.pl>
2020-06-18 18:26:06 +0200

    test zachowania

--- testsReactor/src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
@@ -1,11 +1,15 @@
 package edu.iis.mto.testreactor.atm;
 
+import edu.iis.mto.testreactor.atm.bank.AuthorizationException;
+import edu.iis.mto.testreactor.atm.bank.AuthorizationToken;
 import edu.iis.mto.testreactor.atm.bank.Bank;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.Currency;
@@ -13,7 +17,10 @@ import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class ATMachineTest {
 
     @Mock
@@ -32,10 +39,12 @@ public class ATMachineTest {
     }
 
     @Test
-    public void proper_deposit_and_proper_currency_return_success() throws ATMOperationException {
+    public void proper_deposit_and_proper_currency_return_success() throws ATMOperationException, AuthorizationException {
         Card card = Card.create("2300");
         PinCode pin = PinCode.createPIN(1,2,3,4);
-        Money money = new Money(20);
+        Money money = new Money(0);
+
+        when(bank.autorize("1234","2300")).thenReturn(AuthorizationToken.create("2300"));
         BanknotesPack banknotesPack= BanknotesPack.create(50,Banknote.PL_10);
         List<BanknotesPack> deposit_pack = new ArrayList<>();
         deposit_pack.add(banknotesPack);
@@ -43,11 +52,29 @@ public class ATMachineTest {
 
         Withdrawal result = atMachine.withdraw(pin,card,money);
 
-        BanknotesPack banknotesPackExcepted= BanknotesPack.create(2,Banknote.PL_10);
+        BanknotesPack banknotesPackExcepted= BanknotesPack.create(0,Banknote.PL_10);
         List<BanknotesPack> deposit_pack_expected = new ArrayList<>();
         deposit_pack_expected .add(banknotesPackExcepted);
         Withdrawal expected = Withdrawal.create(deposit_pack_expected);
         assertEquals(expected,result);
+    }
+
+    @Test
+    public void proper_pin_and_card_return_autorization() throws ATMOperationException, AuthorizationException {
+        Card card = Card.create("2300");
+        PinCode pin = PinCode.createPIN(1,2,3,4);
+        Money money = new Money(0);
+
+        when(bank.autorize("1234","2300")).thenReturn(AuthorizationToken.create("2300"));
+        BanknotesPack banknotesPack= BanknotesPack.create(50,Banknote.PL_10);
+        List<BanknotesPack> deposit_pack = new ArrayList<>();
+        deposit_pack.add(banknotesPack);
+        deposit = MoneyDeposit.create(currency,deposit_pack);
+
+        Withdrawal result = atMachine.withdraw(pin,card,money);
+
+        verify(bank).autorize("1234","2300");
+
     }
 
     @Test
